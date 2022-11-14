62b24e05c9c6749da614e6fd43fad18e11be8971
longwatermelon <longwatermelon1@gmail.com>
2022-11-13 22:23:25 -0700

    clean up code

--- main.c
+++ main.c
@@ -26,15 +26,8 @@ typedef struct { float x, y; } vec2;
     { 0.f, 0.f, 1.f } \
 }
 
-vec3 vec3_add(vec3 a, vec3 b)
-{
-    return (vec3){ a.x + b.x, a.y + b.y, a.z + b.z };
-}
-
-vec3 vec3_sub(vec3 a, vec3 b)
-{
-    return (vec3){ a.x - b.x, a.y - b.x, a.z - b.z };
-}
+vec3 vec3_add(vec3 a, vec3 b) { return (vec3){ a.x + b.x, a.y + b.y, a.z + b.z }; }
+vec3 vec3_sub(vec3 a, vec3 b) { return (vec3){ a.x - b.x, a.y - b.x, a.z - b.z }; }
 
 vec3 matmul(float mat[3][3], vec3 p)
 {
@@ -49,10 +42,7 @@ vec2 project(vec3 p)
 {
     p.x /= p.z;
     p.y /= p.z;
-    return (vec2){
-        (p.x + .5f) * SCRSIZE,
-        (p.y + .5f) * SCRSIZE / 2
-    };
+    return (vec2){ (p.x + .5f) * SCRSIZE, (p.y + .5f) * SCRSIZE / 2 };
 }
 
 int main()
@@ -89,28 +79,22 @@ int main()
                 float prot[3][3] = ROTX(phi);
 
                 // Rotate along inner ring
-                vec3 ring_center = center;
-                ring_center.y += in_r + out_r;
+                vec3 ring_center = (vec3){ center.x, center.y + in_r + out_r, center.z };
                 vec3 pos = vec3_add(ring_center, matmul(prot, (vec3){ 0.f, out_r, 0.f }));
 
                 // Rotate along outer ring
-                vec3 tcenter = center;
-                tcenter.z = pos.z;
-                vec3 rel = vec3_sub(pos, tcenter);
-                pos = vec3_add(tcenter, matmul(trot, rel));
+                vec3 tcenter = (vec3){ center.x, center.y, pos.z };
+                pos = vec3_add(tcenter, matmul(trot, vec3_sub(pos, tcenter)));
 
                 // Rotate full donut
-                rel = vec3_sub(pos, center);
-                pos = vec3_add(center, matmul(rotz, matmul(roty, matmul(rotx, rel))));
+                pos = vec3_add(center, matmul(rotz, matmul(roty, matmul(rotx, vec3_sub(pos, center)))));
 
                 vec2 proj = project(pos);
                 mvaddch(proj.y, proj.x, '#');
             }
         }
 
-        angle.x += .1f;
-        angle.y += .08f;
-
+        angle = vec3_add(angle, (vec3){ .1f, .08f, .04f });
         refresh();
         usleep(1e4);
     }
